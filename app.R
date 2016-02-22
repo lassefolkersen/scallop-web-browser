6962fc8139e11789025ad6cc6a33745d2033673a
root <root@ip-172-31-31-1.us-west-2.compute.internal>
2016-02-22 10:21:54 +0000

    Atest

--- app.R
+++ app.R
@@ -1,12 +1,33 @@
 
 
-
-#Generating random naming scheme until we figure out the actual names
+#saving global variables
+#chr lengths #from here https://support.bioconductor.org/p/14766/
 cl<-c(247197891, 242713278, 199439629, 191246650, 180727832,
 			170735623, 158630410, 146252219, 140191642, 135347681,
 			134361903, 132289533, 114110907, 106354309, 100334282,
 			88771793,  78646005,  76106388,  63802660,  62429769,
 			46935585, 49396972, 154908521,  57767721)
+chrLengths<-data.frame(row.names=c(1:22,"X","Y"),sum=cl,number=1:length(cl),endsum=cumsum(cl))
+chrLengths[,"startsum"]<- chrLengths[,"endsum"] - chrLengths[,"sum"]
+
+#Misc small variables
+p_range<-1
+maxPos<-sum(cl)#max(data[,"snp_abs_pos"],na.rm=T)
+base<-2
+
+#colouring scheme
+#Setting colours
+# sum(cl)/1e7 this is 307 (meaning we could divide the genome into 307 1e7 bp chunks. Let's.
+cols<-rainbow(307, s = 1, v = 1, start = 0, end = 1, alpha = 1)
+names(cols) <- as.character(1:length(cols))
+
+
+#gene positions
+load("/home/ubuntu/misc/2014-07-16 gene locations.rdata")
+
+
+
+#Generating random naming scheme until we figure out the actual names
 p<-data.frame(
 	row.names=paste("Phenotype",as.character(1:83)), 
 	pheno_id=as.character(1:83), 
@@ -20,15 +41,13 @@ for(i in 1:nrow(p)){
 phenotypes_vector<-p[,"pheno_id"]
 names(phenotypes_vector) <- rownames(p)
 
-load("/home/ubuntu/misc/2014-07-16 gene locations.rdata")
 
 server <- function(input, output) {
 	
-	output$mainPlot <- renderPlot({ 
-		# Take a dependency on input$goButton
-		
+	#main data gathering function	
+	get_data <- reactive({
 		if(input$goButton == 0){
-			return("")
+			return(NULL)
 		}else{
 			
 			##################################
@@ -41,7 +60,7 @@ server <- function(input, output) {
 			p_value_cutoff <- isolate(input$p_value_cutoff)
 			top_label_count<-isolate(input$top_label_count)
 			phenotype <- isolate(input$phenotype)
-
+			
 			if(!tolower(email) %in% c("lassefolkersen@gmail.com","daniel.ziemek@pfizer.com","anders.malarstig@pfizer.com") ){
 				m<-c(format(Sys.time(),"%Y-%m-%d-%H-%M-%S"),"plot",email)
 				m<-paste(m,collapse="\t")
@@ -82,43 +101,13 @@ server <- function(input, output) {
 					dh<-rbind(dh1,dh2)
 				}
 				
-				GWAS<-dh[dh[,"BP"]>start & dh[,"BP"]<end,]
-				if(nrow(GWAS)==0){stop(paste("No SNPs found around gene",gene))}
+				data<-dh[dh[,"BP"]>start & dh[,"BP"]<end,]
+				if(nrow(data)==0){stop(paste("No SNPs found around gene",gene))}
 				
-				GWAS[,"-log10(P)"] <- -log10(GWAS[,"P" ])
-				rownames(GWAS)<-GWAS[,"SNP"]
-				ylim<-c(0,max(c(5,GWAS[,"-log10(P)"])))
-				xlim<-c(start,end)
-				plot(NULL,
-						 xlim=xlim,
-						 ylim=ylim,
-						 xlab=paste("chr",chr),
-						 ylab="-log10(P)",
-						 main=paste("SNPs around",gene)
-				)
+				data[,"-log10(P)"] <- -log10(data[,"P" ])
+				rownames(data)<-data[,"SNP"]
 				
-				points(
-					x=GWAS[,"BP"],
-					y=GWAS[,"-log10(P)"],
-					pch=19,
-					col="dodgerblue"
-				)
-				
-				#highlight top-3 SNPs
-				tooClose<-vector()
-				tooCloseDist<-4000
-				count<-min(c(nrow(GWAS),top_label_count))
-				for(i in 1:count){
-					snps<-rownames(GWAS)[order(GWAS[,"-log10(P)"],decreasing=T)]
-					if(sum(!snps%in%tooClose)==0)break
-					snp<-snps[!snps%in%tooClose][1]
-					text(x=GWAS[snp,"BP"],y=GWAS[snp,"-log10(P)"],label=snp,adj=0,cex=0.9)
-					tooCloseHere<-GWAS[snp,"BP"] - tooCloseDist < GWAS[,"BP"] & GWAS[snp,"BP"] + tooCloseDist > GWAS[,"BP"]
-					tooClose<-c(tooClose,rownames(GWAS)[tooCloseHere])
-					
-				}
-				lines(x=c(geneLocations[gene,"start"],geneLocations[gene,"end"]),y=c(0,0),lwd=4,col="black")
-				
+				return(data)				
 				
 				##################################
 				#Protein-aspect --- starting with 
@@ -139,22 +128,11 @@ server <- function(input, output) {
 				
 				
 				#calculate the "universal-BP" --- i.e. the BP-sum on chr + all-other-chr before it (=were on the circle to plot)
-				#from here https://support.bioconductor.org/p/14766/
-				cl<-c(247197891, 242713278, 199439629, 191246650, 180727832,
-							170735623, 158630410, 146252219, 140191642, 135347681,
-							134361903, 132289533, 114110907, 106354309, 100334282,
-							88771793,  78646005,  76106388,  63802660,  62429769,
-							46935585, 49396972, 154908521,  57767721)
-				chrLengths<-data.frame(row.names=c(1:22,"X","Y"),sum=cl,number=1:length(cl),endsum=cumsum(cl))
-				chrLengths[,"startsum"]<- chrLengths[,"endsum"] - chrLengths[,"sum"]
 				data[,"snp_abs_pos"]<-chrLengths[data[,"CHR"],"startsum"] + data[,"BP"]
 				data[,"trait_abs_pos"]<-chrLengths[data[,"trait_chr"],"startsum"] + data[,"trait_pos"]
 				
 				
 				#This block transforms all the (chr-pos, -logP) positions from cartesian coordinates to polar coordinates (=this is the circularizing step)
-				p_range<-1
-				maxPos<-sum(cl)#max(data[,"snp_abs_pos"],na.rm=T)
-				base<-2
 				for(i in 1:nrow(data)){
 					data[i,"r"] <- max(c(0,log10(data[i,"neglogp"]) / p_range))
 					data[i,"theta"] <- (data[i,"snp_abs_pos"] / maxPos) * 2 * pi
@@ -166,114 +144,8 @@ server <- function(input, output) {
 				data<-data[!is.na(data[,"x"]),]
 				data<-data[order(data[,"snp_abs_pos"]),]
 				
-				#Setting colours
-				# sum(cl)/1e7 this is 307 (meaning we could divide the genome into 307 1e7 bp chunks. Let's.
-				cols<-rainbow(307, s = 1, v = 1, start = 0, end = 1, alpha = 1)
-				names(cols) <- as.character(1:length(cols))
 				data[,"colours"]<-cols[as.character(round(data[,"snp_abs_pos"]/1e7))]
-				
-				
-				#Open up the actual plotting mechanism with an empty canvas
-				plot(NULL,xlim=c(-4,4),ylim=c(-4,4),ylab="",xlab="",xaxt="n",yaxt="n")
-				
-				#Drawing the lines for the manhattan plot (using polar coordinates from above)
-				for(i in 1:nrow(data)){
-					lines(
-						x=c(data[i,"x"],data[i,"x0"]),
-						y=c(data[i,"y"],data[i,"y0"]),
-						col=data[i,"colours"])
-				}
-				
-				
-				#drawing the frame-work circles (we want a P 0, 10^-8 and a 'current-cutoff-circle)
-				ch<-data.frame(offset=c(0,log10(p_value_cutoff),log10(8)),lty=c(1,2,1),col=c("black","grey70","grey50"),stringsAsFactors=F)
-				for(k in 1:nrow(ch)){
-					res<-50
-					b<-seq(0,2*pi,length.out=res)
-					for(i in 1:(res-1)){
-						x1 = (ch[k,"offset"] + base) * cos( b[i] )
-						x2 = (ch[k,"offset"] + base) * cos( b[i+1] )
-						y1 = (ch[k,"offset"] + base) * sin( b[i] )
-						y2 = (ch[k,"offset"] + base) * sin( b[i+1] )
-						lines(x=c(x1,x2),y=c(y1,y2),lty=ch[k,"lty"],col=ch[k,"col"])
-					}
-				}
-				
-				#drawing the internal lines
-				for(j in 1:nrow(data)){
-					orig<-data[j,"snp_abs_pos"]
-					dest<-data[j,"trait_abs_pos"]
-					colour<-rainbow(nrow(data), s = 1, v = 1, start = 0, end = 1, alpha = 1)[sum(data[,"snp_abs_pos"]<orig)+1]
-					x1 = (base) * cos( 2*pi*(orig/maxPos) )
-					y1 = (base) * sin( 2*pi*(orig/maxPos) )
-					x2 = (base) * cos( 2*pi*(dest/maxPos) )
-					y2 = (base) * sin( 2*pi*(dest/maxPos) )
-					lines(x=c(x1,x2),y=c(y1,y2),col=colour,lwd=1)
-					print(paste(x1,y1,x2,y2))
-				}
-				
-				
-				
-				#drawing the chromosome sizes
-				for(i in 1:nrow(chrLengths)){
-					start<-chrLengths[i,"startsum"]
-					# end<-chrLengths[i,"cumsum"]
-					x1 = (base) * cos( 2*pi*(start/maxPos) )
-					x2 = (base+0.1) * cos( 2*pi*(start/maxPos) )
-					y1 = (base) * sin( 2*pi*(start/maxPos)  )
-					y2 = (base+0.1) * sin( 2*pi*(start/maxPos)  )
-					
-					lines(x=c(x1,x2),y=c(y1,y2),lty=1,col="black")
-					text(x1,y1,label=rownames(chrLengths)[i],cex=0.5,adj=-0.2,col="grey50")
-				}
-				
-
-				#labelling top-X hits
-				tooClose<-vector()
-				tooCloseDist<-20000000
-				count<-min(c(nrow(data),top_label_count))
-				for(i in 1:count){
-					snps<-data[order(data[,"neglogp"],decreasing=T),"SNP"]
-					if(sum(!snps%in%tooClose)==0)break
-					snp<-snps[!snps%in%tooClose][1]
-					x<-data[data[,"SNP"]%in%snp,"x"]
-					y<-data[data[,"SNP"]%in%snp,"y"]
-					text(x=x,y=y,label=snp,adj=0,cex=0.8)
-					pos<-data[data[,"SNP"]%in%snp,"snp_abs_pos"]
-					tooCloseHere<-data[abs(data[,"snp_abs_pos"] - pos) <tooCloseDist,"SNP"]
-					tooClose<-c(tooClose,tooCloseHere)
-				}
-				
-				
-				#drawing a legend
-				cols<-cols[seq(1,length(cols),by=3)]
-				min<-3
-				max<-4
-				scale = (length(cols)-1)/(max-min)
-				# plot(NULL,xlim=c(-4,4),ylim=c(-4,4),ylab="",xlab="",xaxt="n",yaxt="n")
-				for (i in 1:(length(cols)-1)) {
-					y = (i-1)/scale + min
-					rect(-4,y,-3.5,y+1/scale, col=cols[i], border=NA)
-				}
-				text(x=-3.4,y=4.05,"Chr",cex=0.7)
-				
-				for(chr in c(1,2,3,5,7,9,11,13,15,18,22)){
-					f <- chrLengths[chr,"startsum"] / chrLengths[nrow(chrLengths),"endsum"]
-					text(x=-3.4, y=f+min * (max-min),chr,cex=0.5)
-				}
-				legend(
-					x=-3,y=4,
-							 legend=c("P=1e-8",paste("P=1e-",signif(p_value_cutoff,2),sep="")),
-					lty=c(1,2),
-					lwd=c(1,1),
-					col=c("grey50","grey70"),
-					cex=0.7,bty="n"
-				)
-				
-				
-				
-				
-				
+				return(data)				
 				
 			}else if(type == "download"){
 				return(NULL)
@@ -284,13 +156,212 @@ server <- function(input, output) {
 			
 		}
 	})
+	
+	
+	output$mainPlot <- renderPlot({ 
+		email <- isolate(input$email)
+		type <- isolate(input$type)
+		gene <- isolate(input$gene)
+		distance <- isolate(input$distance)
+		p_value_cutoff <- isolate(input$p_value_cutoff)
+		top_label_count<-isolate(input$top_label_count)
+		phenotype <- isolate(input$phenotype)
+		
+		data<-get_data()
+		if(is.null(data))return(NULL)
+		
+		if(type == "dna"){
+			chr<-sub("^chr","",geneLocations[gene,"chr_name"])
+			start<-geneLocations[gene,"start"] - distance
+			end<-geneLocations[gene,"end"] + distance
+			
+			
+			ylim<-c(0,max(c(5,data[,"-log10(P)"])))
+			xlim<-c(start,end)
+			plot(NULL,
+					 xlim=xlim,
+					 ylim=ylim,
+					 xlab=paste("chr",chr),
+					 ylab="-log10(P)",
+					 main=paste("SNPs around",gene)
+			)
+			
+			points(
+				x=data[,"BP"],
+				y=data[,"-log10(P)"],
+				pch=19,
+				col="dodgerblue"
+			)
+			
+			#highlight top-3 SNPs
+			tooClose<-vector()
+			tooCloseDist<-4000
+			count<-min(c(nrow(data),top_label_count))
+			for(i in 1:count){
+				snps<-rownames(data)[order(data[,"-log10(P)"],decreasing=T)]
+				if(sum(!snps%in%tooClose)==0)break
+				snp<-snps[!snps%in%tooClose][1]
+				text(x=data[snp,"BP"],y=data[snp,"-log10(P)"],label=snp,adj=0,cex=0.9)
+				tooCloseHere<-data[snp,"BP"] - tooCloseDist < data[,"BP"] & data[snp,"BP"] + tooCloseDist > data[,"BP"]
+				tooClose<-c(tooClose,rownames(data)[tooCloseHere])
+				
+			}
+			lines(x=c(geneLocations[gene,"start"],geneLocations[gene,"end"]),y=c(0,0),lwd=4,col="black")
+			
+			
+		}else if(type =="protein"){
+			#Open up the actual plotting mechanism with an empty canvas
+			plot(NULL,xlim=c(-4,4),ylim=c(-4,4),ylab="",xlab="",xaxt="n",yaxt="n")
+			
+			#Drawing the lines for the manhattan plot (using polar coordinates from above)
+			for(i in 1:nrow(data)){
+				lines(
+					x=c(data[i,"x"],data[i,"x0"]),
+					y=c(data[i,"y"],data[i,"y0"]),
+					col=data[i,"colours"])
+			}
+			
+			
+			#drawing the frame-work circles (we want a P 0, 10^-8 and a 'current-cutoff-circle)
+			ch<-data.frame(offset=c(0,log10(p_value_cutoff),log10(8)),lty=c(1,2,1),col=c("black","grey70","grey50"),stringsAsFactors=F)
+			for(k in 1:nrow(ch)){
+				res<-50
+				b<-seq(0,2*pi,length.out=res)
+				for(i in 1:(res-1)){
+					x1 = (ch[k,"offset"] + base) * cos( b[i] )
+					x2 = (ch[k,"offset"] + base) * cos( b[i+1] )
+					y1 = (ch[k,"offset"] + base) * sin( b[i] )
+					y2 = (ch[k,"offset"] + base) * sin( b[i+1] )
+					lines(x=c(x1,x2),y=c(y1,y2),lty=ch[k,"lty"],col=ch[k,"col"])
+				}
+			}
+			
+			#drawing the internal lines
+			for(j in 1:nrow(data)){
+				orig<-data[j,"snp_abs_pos"]
+				dest<-data[j,"trait_abs_pos"]
+				colour<-rainbow(nrow(data), s = 1, v = 1, start = 0, end = 1, alpha = 1)[sum(data[,"snp_abs_pos"]<orig)+1]
+				x1 = (base) * cos( 2*pi*(orig/maxPos) )
+				y1 = (base) * sin( 2*pi*(orig/maxPos) )
+				x2 = (base) * cos( 2*pi*(dest/maxPos) )
+				y2 = (base) * sin( 2*pi*(dest/maxPos) )
+				lines(x=c(x1,x2),y=c(y1,y2),col=colour,lwd=1)
+				print(paste(x1,y1,x2,y2))
+			}
+			
+			
+			
+			#drawing the chromosome sizes
+			for(i in 1:nrow(chrLengths)){
+				start<-chrLengths[i,"startsum"]
+				# end<-chrLengths[i,"cumsum"]
+				x1 = (base) * cos( 2*pi*(start/maxPos) )
+				x2 = (base+0.1) * cos( 2*pi*(start/maxPos) )
+				y1 = (base) * sin( 2*pi*(start/maxPos)  )
+				y2 = (base+0.1) * sin( 2*pi*(start/maxPos)  )
+				
+				lines(x=c(x1,x2),y=c(y1,y2),lty=1,col="black")
+				text(x1,y1,label=rownames(chrLengths)[i],cex=0.5,adj=-0.2,col="grey50")
+			}
+			
+			
+			#labelling top-X hits
+			tooClose<-vector()
+			tooCloseDist<-20000000
+			count<-min(c(nrow(data),top_label_count))
+			for(i in 1:count){
+				snps<-data[order(data[,"neglogp"],decreasing=T),"SNP"]
+				if(sum(!snps%in%tooClose)==0)break
+				snp<-snps[!snps%in%tooClose][1]
+				x<-data[data[,"SNP"]%in%snp,"x"]
+				y<-data[data[,"SNP"]%in%snp,"y"]
+				text(x=x,y=y,label=snp,adj=0,cex=0.8)
+				pos<-data[data[,"SNP"]%in%snp,"snp_abs_pos"]
+				tooCloseHere<-data[abs(data[,"snp_abs_pos"] - pos) <tooCloseDist,"SNP"]
+				tooClose<-c(tooClose,tooCloseHere)
+			}
+			
+			
+			#drawing a legend
+			cols<-cols[seq(1,length(cols),by=3)]
+			min<-3
+			max<-4
+			scale = (length(cols)-1)/(max-min)
+			# plot(NULL,xlim=c(-4,4),ylim=c(-4,4),ylab="",xlab="",xaxt="n",yaxt="n")
+			for (i in 1:(length(cols)-1)) {
+				y = (i-1)/scale + min
+				rect(-4,y,-3.5,y+1/scale, col=cols[i], border=NA)
+			}
+			text(x=-3.4,y=4.05,"Chr",cex=0.7)
+			
+			for(chr in c(1,2,3,5,7,9,11,13,15,18,22)){
+				f <- chrLengths[chr,"startsum"] / chrLengths[nrow(chrLengths),"endsum"]
+				text(x=-3.4, y=f+min * (max-min),chr,cex=0.5)
+			}
+			legend(
+				x=-3,y=4,
+				legend=c("P=1e-8",paste("P=1e-",signif(p_value_cutoff,2),sep="")),
+				lty=c(1,2),
+				lwd=c(1,1),
+				col=c("grey50","grey70"),
+				cex=0.7,bty="n"
+			)
+			
+		}else if(type == "download"){
+			return(NULL)
+		}else{ 
+			stop("Aspect not implemented yet")
+		}
+		
+		
+	})
+	
+	output$mainTable <- renderDataTable({ 
+		email <- isolate(input$email)
+		type <- isolate(input$type)
+		gene <- isolate(input$gene)
+		distance <- isolate(input$distance)
+		p_value_cutoff <- isolate(input$p_value_cutoff)
+		top_label_count<-isolate(input$top_label_count)
+		phenotype <- isolate(input$phenotype)
+		
+		data<-get_data()
+		if(type%in%c("dna","protein") & is.null(data))return(NULL)
+		
+		if(type == "dna"){
+			data<-data[,c("SNP","CHR","BP","P")]
+			return(data)
+		}else if(type =="protein"){
+			data<-data[,c("SNP","CHR","BP","P")]
+			return(data)
+		}else if(type == "download"){
+			ip<-"http://52.36.74.109/"
+			allFiles<-list.files("/srv/shiny-server/www")
+			chr<-sub("_pheno.+$","",sub("^.+chr","",allFiles)	)
+			pheno<-sub("\\.txt.+$","",sub("^.+pheno","",allFiles)	)
+			data<-data.frame(
+				chr=chr,
+				phenotype=pheno,
+				link=paste(ip,"www/",allFiles,sep="")
+			)
+			return(data)
+			
+			
+		}else{ 
+			
+			stop("Aspect not implemented yet")
+		}
+		
+		
+	})
+	
 }
 
 
 
 
 ui <- fluidPage(
-	titlePanel("Improve OLINK browser"),
+	titlePanel("Improve-OLINK browser"),
 	sidebarLayout(
 		sidebarPanel(
 			
@@ -305,7 +376,7 @@ ui <- fluidPage(
 				condition = "input.type == 'dna'",
 				textInput(inputId="gene", label = "Gene", value = "")
 			),
-				selectInput("phenotype", "Protein", choices = phenotypes_vector),
+			selectInput("phenotype", "Protein", choices = phenotypes_vector),
 			checkboxInput("advanced", "Advanced options", value = FALSE),
 			conditionalPanel(
 				condition = "input.type == 'dna'  & input.advanced",
@@ -325,7 +396,9 @@ ui <- fluidPage(
 			width=4
 		),
 		mainPanel(
-			plotOutput("mainPlot",width = "800px", height = "800px")
+			plotOutput("mainPlot",width = "800px", height = "800px"),
+			dataTableOutput("mainTable")
+			
 		)
 	)
 )
