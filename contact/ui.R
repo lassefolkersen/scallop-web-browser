e3c1f1a564f499a6d572a543950e438995fb3f39
Lasse Folkersen <FOLK@lundbeck.com>
2017-03-21 09:19:58 +0100

    remove outdata re-captcha

--- contact/ui.R
+++ contact/ui.R
@@ -7,7 +7,7 @@ shinyUI(bootstrapPage(
 	titlePanel("Contact"),
 	beginPage(),
 	HTML(	"
-<b>Analysis and design:</b><br><a href='http://www.cbs.dtu.dk/staff/show-staff.php?id=1202'>Lasse Folkersen</a> (<u><a href='http://www.google.com/recaptcha/mailhide/d?k=01pdzWyCfeU-_1PRAPdKlJfg==&amp;c=3eyQPG-VqkHu6ECGRBSHdRraKCXOUsVtLpuyWWt-dpY=' onclick='window.open('http://www.google.com/recaptcha/mailhide/d?k\07501pdzWyCfeU-_1PRAPdKlJfg\75\75\46c\0753eyQPG-VqkHu6ECGRBSHdRraKCXOUsVtLpuyWWt-dpY\075', '', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=0,width=500,height=300'); return false;' title='Reveal this e-mail address'>email</a></u>).<br><br>
+<b>Analysis and design:</b><br><a href='http://www.cbs.dtu.dk/staff/show-staff.php?id=1202'>Lasse Folkersen</a>.<br><br>
 				
 <b>Project lead:</b><br>Anders Malarstig.<br><br>
 
