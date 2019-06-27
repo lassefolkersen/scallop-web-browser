e97e76d985d03fe640d2186ff51f41ba753cf67c
Lasse Folkersen <FOLK@lundbeck.com>
2019-06-27 11:00:07 +0200

    Don't let the not-possible-to-show proteins results in errors. Just make it impossible to select them from the beginning

--- scallop_pathways/ui.R
+++ scallop_pathways/ui.R
@@ -9,6 +9,12 @@ library("visNetwork")
 proteins<-c('ADM','AGRP','Beta-NGF','CA-125','CASP-8','CCL20','CCL3','CCL4','CD40','CD40-L','CHI3L1','CSF-1','CSTB','CTSD','CTSL1','CX3CL1','CXCL1','CXCL16','CXCL6','Dkk-1','ECP','EGF','EN-RAGE','ESM-1','FABP4','FAS','FGF-23','FS','GAL','Gal-3','GDF-15','GH','HB-EGF','HGF','hK11','HSP_27','IL-18','IL-1ra','IL-27','IL-6','IL-6RA','IL-8','IL16','ITGB1BP2','KIM-1','KLK6','LEP','LOX-1','mAmP','MB','MCP-1','MMP-1','MMP-10','MMP-12','MMP-3','MMP-7','MPO','NEMO','NT-pro_BNP','OPG','PAPPA','PAR-1','PDGF_subunit_B','PECAM-1','PlGF','PSGL-1','PTX3','RAGE','REN','RETN','SCF','SELE','SIRT2','SPON1','ST2','t-PA','TF','TIE2','TM','TNF-R1','TNF-R2','TNFSF14','TRAIL','TRAIL-R2','TRANCE','U-PAR','VEGF-A','VEGF-D')
 
 
+proteins_to_omit <- c('Beta-NGF','CSF-1','ECP','EGF','GH','mAmp','NEMO','NT-pro_BNP')
+#mAmp gave a weird error - the others are just not possible to show
+
+proteins<-proteins[!proteins%in%proteins_to_omit]
+
+
 shinyUI(bootstrapPage(
 	head(),
 	navigation(),
