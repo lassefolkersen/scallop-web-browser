c1e511687ef5bd0e2eac25b749b589e9f47fd6c8
Lasse Folkersen <FOLK@lundbeck.com>
2020-06-24 10:42:56 +0200

    test 3

--- update/server.R
+++ update/server.R
@@ -11,12 +11,12 @@ shinyServer(function(input, output) {
 	  }
 		
 		if(input$goButton > 0){
-	    
+	    original_folder<-getwd()
 	    setwd("/home/ubuntu/srv/scallop-web-browser")
 	    m1<-system("git pull",intern=T)
 	    Sys.sleep(1)
 	  
-	    m2<-paste("Executed update command with this output:",m1)
+	    m2<-paste("Executed update command with this output:",m1,"also note this:",original_folder)
 	    return(m2)  
 	  }
 	  
